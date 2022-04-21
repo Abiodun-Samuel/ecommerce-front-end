c6af6a4f3c5a74c5990ce29df092cce3b00fdcb6
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-04-21 17:23:14 +0100

    place order done

--- src/screens/PlaceorderScreen.js
+++ src/screens/PlaceorderScreen.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Card } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { saveShippingAddress } from "../actions/cartActions";
-import CheckoutSteps from "../components/CheckoutSteps";
+// import { saveShippingAddress } from "../actions/cartActions";
+// import CheckoutSteps from "../components/CheckoutSteps";
 import Message from "../components/Message";
 import SectionHeader from "../components/SectionHeader";
 import { createOrder } from "../actions/orderActions";
+import { Image } from "cloudinary-react";
+import { BsCartCheck } from "react-icons/bs";
+import { toastMessage } from "../utils/utils";
 
 const PlaceorderSreen = () => {
   const dispatch = useDispatch();
@@ -21,7 +23,8 @@ const PlaceorderSreen = () => {
     cart.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0)
   );
   cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 10);
-  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+  cart.taxPrice = 0;
+  // cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
   cart.totalPrice = addDecimals(
     Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)
   );
@@ -41,6 +44,7 @@ const PlaceorderSreen = () => {
         taxPrice: cart.taxPrice,
       })
     );
+    toastMessage("success", "Order has been placed successfully");
     navigate(`/orderlist`);
   };
 
@@ -72,56 +76,82 @@ const PlaceorderSreen = () => {
         <div className="row">
           <div className="col-lg-6 col-md-10 col-sm-12">
             <div className="shipping-box bg-white p-4 shadow rounded">
-             
+              <h6 className="text-primary">Order Details</h6>
+              <hr />
+              <p className="my-0 py-0">
+                <strong>Address: </strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city},
+                {cart.shippingAddress.country}
+                {/* {cart.shippingAddress.postalCode}, */}
+              </p>
+              <p className="my-0 py-0">
+                <strong>Payment Method: </strong> {cart.paymentMethod}
+              </p>
+              <hr />
+              <h6 className="text-primary">Order Items</h6>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
+              ) : (
+                <ul>
+                  {cart.cartItems.map((item, index) => (
+                    <li key={index} className="my-1">
+                      <Image
+                        cloudName="psalmzie"
+                        publicId={item.image}
+                        width="auto"
+                        height="50"
+                        crop="scale"
+                        alt={item.name}
+                        className="img-fluid mr-2"
+                      />
+                      <Link className="mx-1" to={`/product/${item.product}`}>
+                        {item.name}
+                      </Link>
+                      <span>
+                        {item.quantity} x &#8358;{item.price} = &#8358;
+                        {item.quantity * item.price}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              <hr />
+              <h6 className="text-primary">Order Summary</h6>
+              <div className="table-responsive ">
+                <table className="table table-hover shadow-sm">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th scope="col">Items</th>
+                      <th scope="col">Shipping</th>
+                      <th scope="col">Tax</th>
+                      <th scope="col">Total</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td> &#8358;{cart.itemsPrice}</td>
+                      <td> &#8358;{cart.shippingPrice}</td>
+                      <td> &#8358;{cart.taxPrice}</td>
+                      <td> &#8358;{cart.totalPrice}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+              {error && <Message variant="danger">{error}</Message>}
+              <button
+                type="button"
+                className="btn_one w-100 mt-2"
+                disabled={cart.cartItems === 0}
+                onClick={placeOrderHandler}
+              >
+                <BsCartCheck className="mx-2" /> Place order
+              </button>
             </div>
           </div>
         </div>
       </div>
     </>
-    /* <div>
-     
-      <p>
-        <strong>Address:</strong>
-        {cart.shippingAddress.address}, {cart.shippingAddress.city}
-        {cart.shippingAddress.postalCode},{cart.shippingAddress.country}
-      </p>
-      <h4>Payment Method: {cart.paymentMethod}</h4>
-
-      <h2>Order Items</h2>
-      {cart.cartItems.length === 0 ? (
-        <Message>Your cart is empty</Message>
-      ) : (
-        <ul>
-          {cart.cartItems.map((item, index) => (
-            <li key={index}>
-              <img src={item.image} alt={item.name} />
-              <Link to={`/product/${item.product}`}>{item.name}</Link>
-              {item.quantity} x ${item.price} = ${item.quantity * item.price}
-            </li>
-          ))}
-        </ul>
-      )}
-
-      <Card>
-        <h2>Order Summary</h2>
-        <p>Items &#8358;{cart.itemsPrice}</p>
-        <h2>Shipping</h2>
-        <p> &#8358;{cart.shippingPrice}</p>
-        <h2>Tax </h2>
-        <p> &#8358;{cart.taxPrice}</p>
-        <h2>Total</h2>
-        <p> &#8358;{cart.totalPrice}</p>
-      </Card>
-      {error && <Message variant="danger">{error}</Message>}
-      <button
-        type="button"
-        className="btn"
-        disabled={cart.cartItems === 0}
-        onClick={placeOrderHandler}
-      >
-        Place order
-      </button>
-    </div> */
   );
 };
 
