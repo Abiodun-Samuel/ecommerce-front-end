6fa2e4485f8fb82a52393cf012e42321cab37fe1
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-04-24 07:04:01 +0100

    search page

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,36 +8,99 @@ import Loader from "../components/Loader";
 import { Link, useParams } from "react-router-dom";
 import ProductCarousel from "../components/ProductCarousel";
 import Paginate from "../components/Paginate";
+import SearchBox from "../components/SearchBox";
+import Category from "../components/Category";
+import { Carousel } from "bootstrap";
+import { getCategories } from "../actions/categoryActions";
+import hero1 from "../images/bg/hero1.jpg";
+import hero2 from "../images/bg/hero2.jpg";
+import hero3 from "../images/bg/hero3.jpg";
+import SectionHeader from "../components/SectionHeader";
 
 const SearchScreen = () => {
+  const images = [hero1, hero2, hero3];
   const { keyword } = useParams();
   let { pageNumber } = useParams();
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const { loading, error, products, page, pages } = productList;
+  const {
+    loading: loadingCategory,
+    error: errorCategory,
+    categories,
+  } = useSelector((state) => state.categoryList);
+
   React.useEffect(() => {
     dispatch(listProducts(keyword, pageNumber));
+    dispatch(getCategories());
   }, [dispatch, keyword, pageNumber]);
 
   return (
-    <div>
-      {!keyword ? (
-        <ProductCarousel />
-      ) : (
-        <Link className="btn btn-primary" to="/">
-          HomePage
-        </Link>
-      )}
-      <h1>Latest Products</h1>
+    <>
+      <div className="row mt-5 mb-2">
+        <div className="col-lg-4 my-2">
+          <SearchBox />
+          <div className="category-box shadow p-3">
+            <h5>Categories</h5>
+            <Category
+              loadingCategory={loadingCategory}
+              errorCategory={errorCategory}
+              categories={categories}
+            />
+          </div>
+        </div>
+        <div className="col-lg-8 my-2">
+          <div className="hero-img">
+            <img className="img-fluid" src={hero1} alt="search result" />
+          </div>
+        </div>
+      </div>
+
+      <div className="row mt-2">
+        <div className="col-lg-12">
+          <SectionHeader header="Search Result" />
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb p-0 m-0 bg-transparent my-2 small">
+              <li className="breadcrumb-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Search Results
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+
       {loading ? (
-        <h2>
-          <Loader fullPage={true} />
-        </h2>
+        <Loader fullPage={true} />
       ) : error ? (
-        <Message variant="danger">{error}</Message>
+        <Message danger="danger">{error}</Message>
       ) : (
         <>
-          <Row>
+          <div className="row">
+            {products.map((product) => {
+              return (
+                <div
+                  className="col-lg-2 col-md-4 col-sm-6 col-6 my-2"
+                  key={product._id}
+                >
+                  <Product product={product} />
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="row my-2">
+            <div className="col-lg-12">
+              <Paginate
+                page={page}
+                pages={pages}
+                keyword={keyword ? keyword : ""}
+              />
+            </div>
+          </div>
+          {/* <Row>
             {products.map((product) => {
               return (
                 <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
@@ -50,10 +113,10 @@ const SearchScreen = () => {
             page={page}
             pages={pages}
             keyword={keyword ? keyword : ""}
-          />
+          /> */}
         </>
       )}
-    </div>
+    </>
   );
 };
 
